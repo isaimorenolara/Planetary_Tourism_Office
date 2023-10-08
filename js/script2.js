723b842b7f2d919592628cc2277559ebe02fc68a
isaimorenolara <a324591@alumnos.uaslp.mx>
2023-10-07 21:18:22 -0600

    cambios

--- js/script2.js
+++ js/script2.js
@@ -2,9 +2,44 @@ $(document).ready(function () {
     var id = 0;
 
     $.getJSON('../json/planets.json', function (data) {
-        console.log(data);
-        
+        const planetCarousel = $('#planetCarousel');
+        const indicators = $('.carousel-indicators');
+
+        data.forEach(function (planet, index) {
+            const indicator = $('<button>')
+                .attr('type', 'button')
+                .attr('data-bs-target', '#carouselExampleCaptions')
+                .attr('data-bs-slide-to', index)
+                .attr('aria-label', `Slide ${index + 1}`);
+
+            if (index === 0) {
+                indicator.addClass('active'); // Marca el primer indicador como activo
+            }
+
+            indicators.append(indicator);
+
+            const carouselItem = $('<div>').addClass('carousel-item');
+            if (index === 0) {
+                carouselItem.addClass('active'); // Marca el primer planeta como activo
+            }
+
+            const planetImage = $('<img>').addClass('rounded mx-auto d-block img-fluid mb-5 hola').attr('src', planet.image).attr('alt', planet.name);
+
+            const planetDescription = $('<div>').addClass('carousel-caption d-none d-md-block filtro');
+            const planetName = $('<h2>').text(planet.name).addClass('titulo');
+            
+            const planetInfo = $('<p>').text(planet.description);
+
+            carouselItem.append(planetImage);
+            planetDescription.append(planetName, planetInfo);
+            carouselItem.append(planetDescription);
+
+            planetCarousel.append(carouselItem);
+        });
+
+        $('#carouselExampleCaptions').carousel();
     });
+
 
     $('#agregar').on('click', function () {
         var startingPoint = $('#inputStartingPoint').val();
